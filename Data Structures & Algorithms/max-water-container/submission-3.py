1c0d2fa94103d1bd81d2111d6593b8540b1f03cc
Tewofløs <180424489+TheophilusG@users.noreply.github.com>
2026-07-12 15:59:54 -0400

    Add: max-water-container - submission-3

---
+++ Data Structures & Algorithms/max-water-container/submission-3.py
@@ -0,0 +1,68 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+
+       
+        res = 0 
+        l, r = 0, len(heights)-1
+        while l <r:
+            area= (r-l) * min(heights[l], heights[r])
+            #area =  width (r-l) * minimum of the heights[l] or heights[r] 
+        
+            res = max(res, area)
+            #then the max of the all the computed area gets updated 
+
+            if heights[l] < heights[r]: #if the left height is less update the left
+                l+=1
+            elif heights[l] > heights[r]: # if the right is less update the right
+                r-=1
+            else:  # if equal update one of them 
+                r-=1
+        
+        return res
+
+
+        # so the index of heights is the bar
+        # we needed to calculate the width (the distance b/n the bars)
+        # the vaue of heights array is the height of the bar
+        # you need the max area as the solution (max water = max area)
+
+
+       #draft soltution 
+
+        # hlen= len(heights)
+
+        # l = 0
+        # r = hlen-1
+
+        # hset = {}
+
+        # for i in range(hlen):
+        #     hset[i]= heights[i]
+        
+        # # now we have the {bar:height} mapped
+        # # now sort by values 
+
+        # def getvals(item):
+        #     return item[1]
+        
+        # sorted_hset = sorted(hset.items(), key = getvals, reverse = True)
+        
+
+        # key1, val1 = sorted_hset[0]
+        # key2, val2 = sorted_hset[1]
+
+        # print(key1)
+        # print(val1)
+
+
+       
+
+
+        
+
+            
+            
+
+        
+
+        
