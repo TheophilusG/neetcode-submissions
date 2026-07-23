844f06e02f97ba0229c9c5738c363b9b6d68d210
Tewofløs <180424489+TheophilusG@users.noreply.github.com>
2026-07-23 23:56:20 +0400

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.py
@@ -0,0 +1,23 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        #postion of the car array = postion of ith card [i] in miles
+        # speed of the car array = speed[i] speed of the car in m/hour 
+
+        #destination = postion[target] miles 
+
+        carfleet = []
+        pair = [(p, s) for p, s in zip(position, speed)]
+
+        pair.sort(reverse=True)
+        for p, s in pair:
+            carfleet.append((target-p)/s)
+            if len(carfleet) >=2 and carfleet[-1] <=carfleet[-2]:
+                carfleet.pop()
+        return len(carfleet)
+        #for i in range(len(postion)):
+         
+
+
+        
+
+        
