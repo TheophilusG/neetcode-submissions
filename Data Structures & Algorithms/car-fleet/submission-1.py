fcc959c90f5c6ab645a4a69e1232cd44442577c0
Tewofløs <180424489+TheophilusG@users.noreply.github.com>
2026-07-23 22:37:37 +0400

    Add: car-fleet - submission-1

---
+++ Data Structures & Algorithms/car-fleet/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        #postion of the car array = postion of ith card [i] in miles
+        # speed of the car array = speed[i] speed of the car in m/hour 
+
+        #destination = postion[target] miles 
+
+        carfleet = []
+
+        #for i in range(len(postion)):
+         
+
+        
+
+        
